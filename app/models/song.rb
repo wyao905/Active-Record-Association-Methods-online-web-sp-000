7a1c7c51055ff82f055754226c6aee78f507ee93
Warren Yao <warrenyca905@gmail.com>
2019-08-22 23:21:38 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,11 +9,20 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
+<<<<<<< HEAD
     drake = Artist.where("name = ?", "Drake").first
     if !drake
       drake = Artist.create(name: "Drake")
       drake.save
     end
+=======
+    drake = Artist.where("name: ?", "Drake").first
+    if drake.name == nil
+      drake = Artist.create(name: "Drake")
+      drake.save
+    end
+    binding.pry
+>>>>>>> 782ee8d6e303a7bda17f74b38b632d5e0690504f
     drake.songs << self
   end
 end
